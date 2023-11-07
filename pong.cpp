f3160fb75d1ef0831f20d6ac7fb6b7fc5a0dc39b
Grausamkeeit <53028784+Grausamkeeit@users.noreply.github.com>
2023-11-07 12:25:45 +0700

    Translated all comments into English
    Before that, it was a mix of Russian and English.

--- pong.cpp
+++ pong.cpp
@@ -37,39 +37,39 @@ void Setup() {
 void Draw() {
     clear();
     for (int i = 0; i < width; ++i) {
-        mvaddch(0, i, '#');
-        mvaddch(height - 1, i, '#');
+        mvaddch(0, i, '#'); // Top border
+        mvaddch(height - 1, i, '#'); // Bottom border
     }
     for (int i = 1; i < height - 1; ++i) {
-        mvaddch(i, 0, '#');
-        mvaddch(i, width - 1, '#');
+        mvaddch(i, 0, '#'); // Left border
+        mvaddch(i, width - 1, '#'); // Right border
     }
-    mvaddch(ballY, ballX, 'O');
+    mvaddch(ballY, ballX, 'O'); // Ball
     for (int i = 0; i < paddleSize; ++i) {
-        mvaddch(paddle1Y + i, 1, '|');
-        mvaddch(paddle2Y + i, width - 2, '|');
+        mvaddch(paddle1Y + i, 1, '|'); // Left paddle
+        mvaddch(paddle2Y + i, width - 2, '|'); // Right paddle
     }
     char scoreText[40];
-    sprintf(scoreText, "P1 Score: %d | P2 Score: %d", score1, score2);
-    int scorePos = (width - strlen(scoreText)) / 2;
-    mvprintw(0, scorePos, "%s", scoreText);
+    sprintf(scoreText, "P1 Score: %d | P2 Score: %d", score1, score2); // Score text
+    int scorePos = (width - strlen(scoreText)) / 2; // Center position for score
+    mvprintw(0, scorePos, "%s", scoreText); // Print score at the top center
     refresh();
 }
 
 void AnimateFireworks() {
-    int min_fireworks = width / 4; // Минимальное количество фейерверков - 1/4 длины поля
-    int max_fireworks = (3 * width) / 4; // Максимальное количество - 3/4 длины поля
-    int num_fireworks = rand() % (max_fireworks - min_fireworks + 1) + min_fireworks; // Случайное количество фейерверков
+    int min_fireworks = width / 4; // Minimum number of fireworks - 1/4 width of the field
+    int max_fireworks = (3 * width) / 4; // Maximum number - 3/4 width of the field
+    int num_fireworks = rand() % (max_fireworks - min_fireworks + 1) + min_fireworks; // Random number of fireworks
 
     std::vector<std::pair<int, int>> positions(num_fireworks);
     std::vector<int> explosion_heights(num_fireworks);
     std::vector<int> explosion_radii(num_fireworks);
 
-    // Инициализация начальных позиций фейерверков, высот и радиусов взрывов
+    // Initialize starting positions of fireworks, explosion heights, and radii
     for (int i = 0; i < num_fireworks; ++i) {
-        positions[i] = {rand() % width, height - 2}; // Стартуем снизу
-        explosion_heights[i] = rand() % ((height / 2) - (height / 3)) + (height / 3); // Взрыв на случайной высоте, начиная с 1/3 поля
-        explosion_radii[i] = rand() % 3 + 1; // Случайный радиус взрыва от 1 до 3
+        positions[i] = {rand() % width, height - 2}; // Start from the bottom
+        explosion_heights[i] = rand() % ((height / 2) - (height / 3)) + (height / 3); // Explosion at a random height starting from 1/3 of the field
+        explosion_radii[i] = rand() % 3 + 1; // Random explosion radius from 1 to 3
     }
 
     bool exploded[num_fireworks] = {false};
@@ -78,7 +78,7 @@ void AnimateFireworks() {
         clear();
         for (int i = 0; i < num_fireworks; ++i) {
             if (!exploded[i] && positions[i].second <= explosion_heights[i]) {
-                // Рисуем взрыв в форме сферы
+                // Draw explosion in the shape of a sphere
                 for (int y = -explosion_radii[i]; y <= explosion_radii[i]; y++) {
                     for (int x = -explosion_radii[i]; x <= explosion_radii[i]; x++) {
                         if (x * x + y * y <= explosion_radii[i] * explosion_radii[i]) {
@@ -92,31 +92,31 @@ void AnimateFireworks() {
                 }
                 exploded[i] = true;
             } else if (!exploded[i]) {
-                // Рисуем поднимающийся фейерверк
+                // Draw ascending firework
                 mvaddch(positions[i].second, positions[i].first, '*');
-                positions[i].second--; // Фейерверк поднимается вверх
+                positions[i].second--; // Firework ascends
                 all_exploded = false;
             }
         }
         refresh();
         if (all_exploded) {
-            usleep(1000000); // Задержка 1 секунду после взрыва всех фейерверков
+            usleep(1000000); // Delay 1 second after all fireworks have exploded
             break;
         }
-        usleep(200000); // Задержка между обновлениями анимации
+        usleep(200000); // Delay between animation updates
     }
 }
 
 void DisplayWinner() {
     clear();
     const char* winnerText = score1 >= scoreToWin ? "AI 1 (Left) Wins!" : "AI 2 (Right) Wins!";
-    mvprintw(height / 2, (width - strlen(winnerText)) / 2, "%s", winnerText);
+    mvprintw(height / 2, (width - strlen(winnerText)) / 2, "%s", winnerText); // Print winner text
     refresh();
-    usleep(2000000); // Пауза перед фейерверком
+    usleep(2000000); // Pause before the fireworks
 
-    // Запуск анимации фейерверков
+    // Start fireworks animation
     AnimateFireworks();
-    getch(); // Ожидаем нажатия клавиши для выхода
+    getch(); // Wait for a key press to exit
 }
 
 void MovePaddleAI(int &paddleY, int ballY, bool isStronger, int ballDirX) {
@@ -125,13 +125,13 @@ void MovePaddleAI(int &paddleY, int ballY, bool isStronger, int ballDirX) {
     int delay = isStronger ? strongAIDelay : weakAIDelay;
     int &delayCounter = isStronger ? delayCounterStrong : delayCounterWeak;
 
-    // Время реакции на движение мяча
+    // Reaction time to ball movement
     if (++delayCounter >= delay) {
-        // Перемещение вниз, если мяч ниже ракетки
+        // Move down if the ball is below the paddle
         if (paddleY + paddleSize / 2 < ballY && paddleY + paddleSize <= height - 2) {
             paddleY++;
         }
-        // Перемещение вверх, если мяч выше ракетки
+        // Move up if the ball is above the paddle
         else if (paddleY + paddleSize / 2 > ballY && paddleY > 1) {
             paddleY--;
         }
@@ -147,9 +147,12 @@ void InputAI() {
 void Logic() {
     ballX += ballDirX;
     ballY += ballDirY;
+    // Bounce off the top and bottom
     if (ballY == 1 || ballY == height - 2) ballDirY = -ballDirY;
+    // Bounce off paddles
     if (ballX == 2 && ballY >= paddle1Y && ballY < paddle1Y + paddleSize) ballDirX = -ballDirX;
     if (ballX == width - 3 && ballY >= paddle2Y && ballY < paddle2Y + paddleSize) ballDirX = -ballDirX;
+    // Scoring
     if (ballX == 1) {
         score2++;
         if (score2 >= scoreToWin) return;
@@ -163,12 +166,12 @@ void Logic() {
 }
 
 int main() {
-    initscr();
-    cbreak();
-    noecho();
-    curs_set(0);
-    keypad(stdscr, TRUE);
-    nodelay(stdscr, TRUE);
+    initscr(); // Initialize ncurses
+    cbreak(); // Disable line buffering
+    noecho(); // Don't echo keypresses
+    curs_set(0); // Hide cursor
+    keypad(stdscr, TRUE); // Enable keyboard mapping
+    nodelay(stdscr, TRUE); // Make getch non-blocking
 
     bool gameOver = false;
     while (!gameOver) {
@@ -177,19 +180,19 @@ int main() {
             Draw();
             InputAI();
             Logic();
-            if (getch() == 'q') {
+            if (getch() == 'q') { // Press 'q' to quit
                 gameOver = true;
                 break;
             }
-            usleep(50000);
+            usleep(50000); // 50 ms delay
         }
         if (!gameOver) {
             DisplayWinner();
-            score1 = 0;
+            score1 = 0; // Reset scores
             score2 = 0;
         }
     }
 
-    endwin();
+    endwin(); // End ncurses mode
     return 0;
 }
